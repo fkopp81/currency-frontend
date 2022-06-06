e749ea6e1b1fd228064377c81b33e3e2e68233df
Florian Kopp <floriankopp@posteo.de>
2022-06-06 08:50:44 +0000

    test: Add red tests for historic conversions

--- src/20_Conversions/3_domain/tests/Conversions.test.ts
+++ src/20_Conversions/3_domain/tests/Conversions.test.ts
@@ -5,6 +5,7 @@ import { IConversions } from "../interfaces/IConversions";
 import { IPersistance} from "99_Persistance/4_infrastructure";
 import { mock } from 'jest-mock-extended';
 import { IConversionAPI } from "20_Conversions/4_infrastructure/interfaces/IConversionAPI";
+import mockConversionHistory from "./mockIConversionHistory";
 
 describe("Conversion", () =>
 {
@@ -31,5 +32,142 @@ describe("Conversion", () =>
     const rate = conversions.getCurrentRate(ECurrency.USD, ECurrency.CHF);
     // Assert
     expect(rate).toBe(mockConversion.rates[ECurrency.CHF]);
+    // No persistance for current rates (yet)
+    expect(persistance.load).toBeCalledTimes(0);
+    expect(persistance.save).toBeCalledTimes(0);
+  })
+
+  test("getHistoricRate from API when date not persisted", () =>
+  {
+        // Arrange
+    const conversionFromAPI: IConversion = {
+      base: ECurrency.USD,
+      rates: {
+        [ECurrency.EUR]: 0.01,
+        [ECurrency.USD]: 1,
+        [ECurrency.CHF]: 0.001
+      }
+    };
+    const testDate = "2013-05-30";
+    const conversionHistoryFromPersistance = mockConversionHistory;
+    if (conversionHistoryFromPersistance[testDate])
+    {
+      throw new Error(`date ${testDate} must not exist in mockConversionHistory`);
+    }
+
+    const config = {
+      persistanceKeys: { conversions: "testKey" }
+    } as IConfig;
+    const persistance = mock<IPersistance>();
+    persistance.load.mockReturnValue(conversionHistoryFromPersistance);
+    const conversionAPI = mock<IConversionAPI>();
+    conversionAPI.getHistoricRates.mockReturnValue(conversionFromAPI);
+    const conversions: IConversions = new Conversions(config, conversionAPI, persistance);
+    
+    // Act
+    const rate = conversions.getHistoricRate(ECurrency.USD, ECurrency.CHF, testDate);
+
+    // Assert
+    expect(rate).toBe(conversionFromAPI.rates[ECurrency.CHF]);
+    expect(persistance.load).toBeCalled();
+    expect(persistance.save).toBeCalled();
+  })
+
+  test("getHistoricRate from API when base currency not persisted", () =>
+  {
+    // Arrange
+    const testDate = "2022-05-31";
+    const testBase = ECurrency.USD;
+    const testTo = ECurrency.CHF;
+
+    const conversionFromAPI: IConversion = {
+      base: ECurrency.USD,
+      rates: {
+        [ECurrency.EUR]: 0.01,
+        [ECurrency.USD]: 1,
+        [ECurrency.CHF]: 0.001
+      }
+    };
+    
+    const conversionHistoryFromPersistance = mockConversionHistory;
+    if (!conversionHistoryFromPersistance[testDate])
+    {
+      throw new Error(`date ${testDate} must exist in mockConversionHistory`);
+    }
+    if (!conversionHistoryFromPersistance[testDate][testBase])
+    {
+      throw new Error(`base ${ECurrency[testBase]} must not exist in mockConversionHistory on date ${testDate}`);
+    }
+
+    const config = {
+      persistanceKeys: { conversions: "testKey" }
+    } as IConfig;
+    const persistance = mock<IPersistance>();
+    persistance.load.mockReturnValue(conversionHistoryFromPersistance);
+    const conversionAPI = mock<IConversionAPI>();
+    conversionAPI.getHistoricRates.mockReturnValue(conversionFromAPI);
+    const conversions: IConversions = new Conversions(config, conversionAPI, persistance);
+    
+    // Act
+    const rate = conversions.getHistoricRate(testBase, testTo, testDate);
+
+    // Assert
+    expect(rate).toBe(conversionFromAPI.rates[testTo]);
+    expect(persistance.load).toBeCalled();
+    expect(persistance.save).toBeCalled();
+  })
+
+  test("getHistoricRate from persistance", () =>
+  {
+    // Arrange
+    const testDate = "2022-05-31";
+    const testBase = ECurrency.EUR;
+    const testTo = ECurrency.CHF;
+
+    const conversionFromAPI: IConversion = {
+      base: ECurrency.EUR,
+      rates: {
+        [ECurrency.EUR]: 1,
+        [ECurrency.USD]: 0.0002,
+        [ECurrency.CHF]: 2
+      }
+    };
+    
+    const conversionHistoryFromPersistance = mockConversionHistory;
+    if (!conversionHistoryFromPersistance[testDate])
+    {
+      throw new Error(`date ${testDate} must exist in mockConversionHistory`);
+    }
+    if (!conversionHistoryFromPersistance[testDate][testBase])
+    {
+      throw new Error(`base ${ECurrency[testBase]} must exist in mockConversionHistory on date ${testDate}`);
+    }
+    if (!conversionHistoryFromPersistance[testDate][testBase]!.rates[testTo])
+    {
+      throw new Error(`rate for ${ECurrency[testTo]} with base ${ECurrency[testBase]} must exist in mockConversionHistory on date ${testDate}`);
+    }
+    if (conversionHistoryFromPersistance[testDate][testBase]!.rates[testTo] === conversionFromAPI.rates[testTo])
+    {
+      throw new Error(`rate for ${ECurrency[testTo]} needs to be different between persistance and API mocks`)
+    }
+    const expectedRate = conversionHistoryFromPersistance[testDate][testBase]!.rates[testTo];
+
+    const config = {
+      persistanceKeys: { conversions: "testKey" }
+    } as IConfig;
+    const persistance = mock<IPersistance>();
+    persistance.load.mockReturnValue(conversionHistoryFromPersistance);
+    const conversionAPI = mock<IConversionAPI>();
+    conversionAPI.getHistoricRates.mockReturnValue(conversionFromAPI);
+    const conversions: IConversions = new Conversions(config, conversionAPI, persistance);
+    
+    // Act
+    const rate = conversions.getHistoricRate(testBase, testTo, testDate);
+
+    // Assert
+    expect(rate).toBe(conversionHistoryFromPersistance[testDate][testBase]!.rates[testTo]);
+    expect(persistance.load).toBeCalled();
+    expect(conversionAPI.getHistoricRates).toBeCalledTimes(0);
+    expect(persistance.save).toBeCalledTimes(0);
   })
 })
