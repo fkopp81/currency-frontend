e3cec440df652f2dfa4c6954b7f657f4ce299569
Florian Kopp <floriankopp@posteo.de>
2022-06-25 13:37:54 +0000

    feat: Add basic conversions component

---
+++ src/20_Conversions/1_presentation/implementations/Conversions.tsx
@@ -0,0 +1,9 @@
+const Conversions = () =>
+{
+  return <form aria-label="Conversions">
+    <label>From: <select aria-label="from"></select></label>
+    <label>To: <select aria-label="to"></select></label>
+  </form>
+}
+
+export default Conversions;
