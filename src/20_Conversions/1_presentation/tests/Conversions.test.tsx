8f12536e7b6bd733b6397004b8eaf96e007745ff
Florian Kopp <floriankopp@posteo.de>
2022-06-25 13:37:26 +0000

    test: Add red input elements test

--- src/20_Conversions/1_presentation/tests/Conversions.test.tsx
+++ src/20_Conversions/1_presentation/tests/Conversions.test.tsx
@@ -1,15 +1,26 @@
 import { render, screen } from "@testing-library/react";
+import Conversions from "../implementations/Conversions";
 
 describe("Conversions", () =>
 {
-  test("renders a Conversions class component", () =>
+  test("renders a Conversions component", () =>
   {
     // Arrange
     // Act
-    const renderResult = render(<Conversions />)
+    render(<Conversions />)
     const ConversionsElement = screen.getByRole("form")
     // Assert
     expect(ConversionsElement).toBeTruthy();
-    expect(ConversionsElement.classList).toContain("Conversions")
+    expect(ConversionsElement.getAttribute("aria-label")).toBe("Conversions");
   })
+  test("renders comboBoxes for from-to conversions", () => {
+    // Arrange
+    render(<Conversions />);
+    const comboBoxes = screen.getAllByRole("combobox");
+    // Act
+    // Assert
+    expect(comboBoxes.length).toBe(2);
+    expect(comboBoxes.find((comboBox) => comboBox.getAttribute("aria-label") === "from")).toBeTruthy();
+    expect(comboBoxes.find((comboBox) => comboBox.getAttribute("aria-label") === "to")).toBeTruthy();
+  });
 });
