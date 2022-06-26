a1616d59a11fe40bcdc90bfdf49576a76bbcee22
Florian Kopp <floriankopp@posteo.de>
2022-06-26 07:05:32 +0000

    feat: Add submit button

--- src/20_Conversions/1_presentation/implementations/Conversions.tsx
+++ src/20_Conversions/1_presentation/implementations/Conversions.tsx
@@ -3,6 +3,7 @@ const Conversions = () =>
   return <form aria-label="Conversions">
     <label>From: <select aria-label="from"></select></label>
     <label>To: <select aria-label="to"></select></label>
+    <input type="submit" value="Submit"></input>
     <label>Rate: <output></output></label>
   </form>
 }
