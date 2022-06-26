a6bf2c01d9ece0340c97bc4978f47d92f8c990fd
Florian Kopp <floriankopp@posteo.de>
2022-06-26 13:18:19 +0000

    refactor: Move boilerplate to columns

--- src/0_setup/0_startup/index.ts
+++ src/0_setup/0_startup/index.ts
@@ -1,2 +1,10 @@
 // Cell facade
-export { }
+import "reflect-metadata";
+import "./DI";
+import "0_setup/1_presentation";
+import reportWebVitals from '110_WebVitals/4_infrastructure/reportWebVitals';
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
---
+++ src/0_setup/1_presentation/index.tsx
@@ -0,0 +1,20 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { App } from "1_App/1_presentation";
+
+console.log("Rendering root")
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+
