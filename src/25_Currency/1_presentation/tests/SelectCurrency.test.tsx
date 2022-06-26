f5138072425a7b2eeff78d70f4279945e22ca854
Florian Kopp <floriankopp@posteo.de>
2022-06-26 09:08:08 +0000

    test: Add red test for SelectCurrency options

--- src/25_Currency/1_presentation/tests/SelectCurrency.test.tsx
+++ src/25_Currency/1_presentation/tests/SelectCurrency.test.tsx
@@ -1,3 +1,4 @@
+import { ECurrency } from "25_Currency/5_objects";
 import { render, screen } from "@testing-library/react";
 import SelectCurrency from "../implementations/SelectCurrency";
 
@@ -9,6 +10,22 @@ describe('SelectCurrency', () => {
     // Act
     // Assert
     expect(selectCurrency).toBeDefined();
+  });
+  test("contains options for all currencies and unselected", () => {
+    // Arrange
+    render(<SelectCurrency label="Test:" ariaLabel="test" />);
+    const options = screen.getAllByRole("option");
+    // Act
+    // Assert
+    expect(options.length).toBe(Object.keys(ECurrency).length / 2 + 1);
+    expect(options[0]).toHaveAttribute("value", "");
+    for (const currency in ECurrency)
+    {
+      if (isNaN(Number(currency))) return;
+      expect(
+        options.find((option) => option.getAttribute("value") === currency))
+        .toBeDefined();
+    }
   });
   
 });
