df60993cd77e0a71eeb361cbeacb7d7394411fa7
Florian Kopp <floriankopp@posteo.de>
2022-06-26 07:05:09 +0000

    feat: Add output field

--- src/20_Conversions/1_presentation/implementations/Conversions.tsx
+++ src/20_Conversions/1_presentation/implementations/Conversions.tsx
@@ -1,8 +1,9 @@
 const Conversions = () =>
-{
+{ 
   return <form aria-label="Conversions">
     <label>From: <select aria-label="from"></select></label>
     <label>To: <select aria-label="to"></select></label>
+    <label>Rate: <output></output></label>
   </form>
 }
 
