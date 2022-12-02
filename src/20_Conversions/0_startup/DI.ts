9370d690ddbb4bdd5931aefe1b1f2548fcd1b569
Florian <floriankopp@posteo.de>
2022-12-02 15:07:27 +0000

    refactor: make injectable hook

--- src/20_Conversions/0_startup/DI.ts
+++ src/20_Conversions/0_startup/DI.ts
@@ -1,3 +1,7 @@
+import {
+  DUseSubmitHandler,
+  useSubmitHandler,
+} from "20_Conversions/1_presentation/implementations/useSubmitHandler";
 import { DConversions } from "20_Conversions/3_domain";
 import Conversions from "20_Conversions/3_domain/implementations/Conversions";
 import ConversionAPI from "20_Conversions/4_infrastructure/implementations/ConversionAPI";
@@ -6,3 +10,4 @@ import { container } from "tsyringe";
 
 container.register(DConversions, { useClass: Conversions });
 container.register(DConversionAPI, { useClass: ConversionAPI });
+container.register(DUseSubmitHandler, { useValue: useSubmitHandler });
--- src/20_Conversions/1_presentation/implementations/Conversions.tsx
+++ src/20_Conversions/1_presentation/implementations/Conversions.tsx
@@ -1,36 +1,43 @@
 import { DConversions, IConversions } from "20_Conversions/3_domain";
 import { SelectCurrency } from "25_Currency/1_presentation";
 import { ECurrency } from "25_Currency/5_objects";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { container } from "tsyringe";
+import { useSubmitHandler } from "./useSubmitHandler";
 
-interface IProps
-{
-  conversions?: IConversions
+interface IProps {
+  conversions?: IConversions;
 }
 
-export const Conversions = ({conversions = container.resolve(DConversions)}: IProps) =>
-{ 
+export const Conversions = ({
+  conversions = container.resolve(DConversions),
+}: IProps) => {
   if (!conversions) throw new Error("Internal app error");
-  const [fromCurrency, setFromCurrency] = useState<ECurrency | undefined>(undefined);
-  const [toCurrency, setToCurrency] = useState<ECurrency | undefined>(undefined);
+  const [fromCurrency, setFromCurrency] = useState<ECurrency | undefined>(
+    undefined
+  );
+  const [toCurrency, setToCurrency] = useState<ECurrency | undefined>(
+    undefined
+  );
   const [output, setOutput] = useState<string>("");
-  const submitHandler = useCallback(async () =>
-  {
-    if (fromCurrency === undefined || toCurrency === undefined) return false;
-    const rate = await conversions.getCurrentRate(fromCurrency, toCurrency);
-    setOutput(`${rate}`);
-    return false;
-  }, [conversions, fromCurrency, toCurrency])
-  return <form aria-label="Conversions">
-    <SelectCurrency label="From:" ariaLabel="from" onSelect={setFromCurrency}/>
-    <SelectCurrency label="To:" ariaLabel="to" onSelect={setToCurrency}/>
-    <input
-      type="button"
-      value="Submit"
-      disabled={fromCurrency === undefined || toCurrency === undefined}
-      onClick={submitHandler}
-    />
-    <label>Rate: <output>{output}</output></label>
-  </form>
-}
+  const submitHandler = useSubmitHandler(fromCurrency, toCurrency, setOutput);
+  return (
+    <form aria-label="Conversions">
+      <SelectCurrency
+        label="From:"
+        ariaLabel="from"
+        onSelect={setFromCurrency}
+      />
+      <SelectCurrency label="To:" ariaLabel="to" onSelect={setToCurrency} />
+      <input
+        type="button"
+        value="Submit"
+        disabled={fromCurrency === undefined || toCurrency === undefined}
+        onClick={submitHandler}
+      />
+      <label>
+        Rate: <output>{output}</output>
+      </label>
+    </form>
+  );
+};
